2b260248f6ce2b5d683023a725e457546234a2cb
hung168 <hungbui510@gmail.com>
2022-08-28 22:16:29 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiVietHungBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
